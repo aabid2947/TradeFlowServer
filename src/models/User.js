9bd50eb6e49c7c9ea4dc72ca34c25b98a006947f
aabid2947 <aabidhussainpas@gmail.com>
2025-09-14 19:57:18 +0530

    completed the onboarding setup

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: [true, 'Username is required'],
+  
     unique: true,
     trim: true,
     minlength: [3, 'Username must be at least 3 characters long'],
